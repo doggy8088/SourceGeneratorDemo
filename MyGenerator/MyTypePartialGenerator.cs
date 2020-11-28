96d3d6426c281c2b20e851e3700bd87403f4b7c9
Will <doggy.huang@gmail.com>
2020-11-28 19:10:38 +0800

    Add SourceGenerator required NuGet package and apply attributes & implement inteface on MyTypePartialGenerator class.

--- MyGenerator/MyTypePartialGenerator.cs
+++ MyGenerator/MyTypePartialGenerator.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using System;
 
 namespace MyGenerator
 {
-    public class MyTypePartialGenerator
+    [Generator]
+    public class MyTypePartialGenerator : ISourceGenerator
     {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
